f8d4650c8b311dffd5020755e98cfee1da42dbec
Krzysztof Zielonka <aknolezik@gmail.com>
2026-09-17 16:38:50 +0200

    fix(treesitter): upgrade nvim-treesitter to main branch for Neovim 0.12 compatibility

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,38 +1,45 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile" },
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "go",
-        "gomod",
-        "gosum",
-        "gowork",
-        "typescript",
-        "javascript",
-        "tsx",
-        "ruby",
-        "lua",
-        "luadoc",
-        "json",
-        "yaml",
-        "toml",
-        "html",
-        "css",
-        "markdown",
-        "markdown_inline",
-        "bash",
-        "vim",
-        "vimdoc",
-        "dockerfile",
-      },
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
+    local ts = require("nvim-treesitter")
+    ts.setup({
+      install_dir = vim.fn.stdpath("data") .. "/site",
+    })
+
+    local parsers = {
+      "go",
+      "gomod",
+      "gosum",
+      "gowork",
+      "typescript",
+      "javascript",
+      "tsx",
+      "ruby",
+      "lua",
+      "luadoc",
+      "json",
+      "yaml",
+      "toml",
+      "html",
+      "css",
+      "markdown",
+      "markdown_inline",
+      "bash",
+      "vim",
+      "vimdoc",
+      "dockerfile",
+    }
+    ts.install(parsers)
+
+    -- Enable Treesitter syntax highlighting for all supported buffers
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("user_treesitter_highlight", { clear = true }),
+      callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
+      end,
     })
   end,
 }
